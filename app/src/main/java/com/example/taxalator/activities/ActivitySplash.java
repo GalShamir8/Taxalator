96c2de644497fcacc6e2c759e8844fc0fc01aa91
Galshamir8 <galshamir8@gmail.com>
2022-11-28 20:55:50 +0200

    Add designed icon to splash page

--- app/src/main/java/com/example/taxalator/activities/ActivitySplash.java
+++ app/src/main/java/com/example/taxalator/activities/ActivitySplash.java
@@ -2,12 +2,14 @@ package com.example.taxalator.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.taxalator.R;
+import com.google.android.material.button.MaterialButton;
 
 public class ActivitySplash extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +18,15 @@ public class ActivitySplash extends AppCompatActivity {
     }
 
     private void initViews() {
+        MaterialButton splash_BTN_getStarted = findViewById(R.id.splash_BTN_getStarted);
+        splash_BTN_getStarted.setOnClickListener(e -> openNewPage());
+        ImageView splash_ING_ilus =  findViewById(R.id.splash_ING_ilus);
+        splash_ING_ilus.setImageResource(R.drawable.app_ilustration);
+        ImageView main_IMG_icon =  findViewById(R.id.main_IMG_icon);
+        splash_ING_ilus.setImageResource(R.drawable.taxalator_icon);
+    }
+
+    private void openNewPage() {
+        startActivity(new Intent(this, BaseActivity.class));
     }
 }
