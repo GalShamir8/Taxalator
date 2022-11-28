1085d1bd65e5f57e438f567886dbbfe0d35bc779
Galshamir8 <galshamir8@gmail.com>
2022-11-28 21:23:36 +0200

    Add title

--- app/src/main/java/com/example/taxalator/activities/ActivitySplash.java
+++ app/src/main/java/com/example/taxalator/activities/ActivitySplash.java
@@ -1,13 +1,15 @@
 package com.example.taxalator.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
 import android.widget.ImageView;
+
 
 import com.example.taxalator.R;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
 
 public class ActivitySplash extends AppCompatActivity {
     @Override
@@ -24,6 +26,7 @@ public class ActivitySplash extends AppCompatActivity {
         splash_ING_ilus.setImageResource(R.drawable.app_ilustration);
         ImageView splash_IMG_icon =  findViewById(R.id.splash_IMG_icon);
         splash_IMG_icon.setImageResource(R.drawable.taxalator_icon);
+        MaterialTextView splash_LBL_title = findViewById(R.id.splash_LBL_title);
     }
 
     private void openNewPage() {
