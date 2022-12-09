1bb3da426d6391153572ca64feb280ea51b1ca89
Galshamir8 <galshamir8@gmail.com>
2022-12-09 15:57:33 +0200

    Start buillding custom app bar

---
+++ app/src/main/java/com/example/taxalator/activities/AppBar.java
@@ -0,0 +1,16 @@
+package com.example.taxalator.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.taxalator.R;
+
+public class AppBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_bar);
+    }
+}
