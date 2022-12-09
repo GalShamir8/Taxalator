0a940b96cf8fbbda1b21719cf3bb6e9770e454c9
Galshamir8 <galshamir8@gmail.com>
2022-12-09 17:23:37 +0200

    Add injection of fregmant tinto frame layout in base activity

--- app/src/main/java/com/example/taxalator/activities/BaseActivity.java
+++ app/src/main/java/com/example/taxalator/activities/BaseActivity.java
@@ -1,6 +1,7 @@
 package com.example.taxalator.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -12,5 +13,12 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+        initView();
+    }
+
+    private void initView() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.base_form, new FormFragment());
+        transaction.commit();
     }
 }
