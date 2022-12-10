70a05c5198345355b3ebb6539aeff65441b03ebe
Galshamir8 <galshamir8@gmail.com>
2022-12-10 19:17:33 +0200

    Add callback setters to form fragment

--- app/src/main/java/com/example/taxalator/activities/BaseActivity.java
+++ app/src/main/java/com/example/taxalator/activities/BaseActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.taxalator.R;
 import com.example.taxalator.fragments.FormFragment;
@@ -18,8 +19,14 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        FormFragment formFragment = new FormFragment();
+        formFragment.setOnFinish(e -> calculationSubmittedAction());
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.base_form, new FormFragment());
+        transaction.replace(R.id.base_form, formFragment);
         transaction.commit();
     }
+
+    private void calculationSubmittedAction() {
+    }
+
 }
