ba98a3747985b51a51ffb0c2a20144343e611a29
Galshamir8 <galshamir8@gmail.com>
2022-12-10 20:41:18 +0200

    first implementation for app ads

--- app/src/main/java/com/example/taxalator/activities/BaseActivity.java
+++ app/src/main/java/com/example/taxalator/activities/BaseActivity.java
@@ -1,15 +1,21 @@
 package com.example.taxalator.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
 import com.example.taxalator.R;
 import com.example.taxalator.fragments.FormFragment;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class BaseActivity extends AppCompatActivity {
+    private AdView adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,24 @@ public class BaseActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        openForm();
+        addAppAdds();
+    }
+
+    private void addAppAdds() {
+        adView = new AdView(this);
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+            }
+        });
+
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+    }
+
+    private void openForm() {
         FormFragment formFragment = new FormFragment();
         formFragment.setOnFinish(e -> calculationSubmittedAction());
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
