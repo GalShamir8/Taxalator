4cc4a5ca39bea50d47da81fb6190ab21fd534ce8
Galshamir8 <galshamir8@gmail.com>
2023-01-28 13:17:06 +0200

    Add ads fullt screen basic impl commentout

--- app/src/main/java/com/example/taxalator/activities/BaseActivity.java
+++ app/src/main/java/com/example/taxalator/activities/BaseActivity.java
@@ -12,9 +12,12 @@ import com.example.taxalator.fragments.FormFragment;
 import com.example.taxalator.helpers.CalculationHelper;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 import com.google.android.material.textview.MaterialTextView;
 
 import java.util.Map;
@@ -24,6 +27,10 @@ public class BaseActivity extends AppCompatActivity {
     private AdView adView;
     private MaterialTextView base_LBL_result;
     private FormFragment formFragment;
+
+
+    // full screen
+    private InterstitialAd mInterstitialAd;
 
 
     @Override
@@ -38,7 +45,7 @@ public class BaseActivity extends AppCompatActivity {
         openForm();
         addAppAdds();
     }
-
+// banner
     private void addAppAdds() {
         adView = new AdView(this);
         MobileAds.initialize(this, new OnInitializationCompleteListener() {
@@ -51,6 +58,34 @@ public class BaseActivity extends AppCompatActivity {
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
     }
+
+// full screen
+//    private void addAppAdds() {
+//        adView = new AdView(this);
+//        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+//            @Override
+//            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+//            }
+//        });
+//
+////        adView = findViewById(R.id.adView);
+//        AdRequest adRequest = new AdRequest.Builder().build();
+//        InterstitialAd.load(this,"ca-app-pub-3940256099942544/1033173712", adRequest,
+//                new InterstitialAdLoadCallback() {
+//                    @Override
+//                    public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+//                        // The mInterstitialAd reference will be null until
+//                        // an ad is loaded.
+//                        mInterstitialAd = interstitialAd;
+//                    }
+//                    @Override
+//                    public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+//                        // Handle the error
+//                        mInterstitialAd = null;
+//                    }
+//                });
+////        adView.loadAd(adRequest);
+//    }
 
     private void openForm() {
         formFragment = new FormFragment();
