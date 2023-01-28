dcfd7a0e9e24619f0b8f33ce7d5f4af61623faa5
Galshamir8 <galshamir8@gmail.com>
2023-01-28 13:59:29 +0200

    Add calculation logic

--- app/src/main/java/com/example/taxalator/helpers/CalculationHelper.java
+++ app/src/main/java/com/example/taxalator/helpers/CalculationHelper.java
@@ -6,9 +6,18 @@ public class CalculationHelper {
     private double creditPoints;
 
     public CalculationHelper(double baseSalary, double pension, double creditPoints) {
-        this.baseSalary = baseSalary;
+        this.baseSalary = baseSalary * 12;
+        if (baseSalary == 0){
+            this.baseSalary = 1;
+        }
         this.pension = pension;
+        if (pension == 0){
+            this.pension = 1;
+        }
         this.creditPoints = creditPoints;
+        if (creditPoints == 0){
+            this.creditPoints = 1;
+        }
     }
 
     public double getBaseSalary() {
@@ -36,6 +45,71 @@ public class CalculationHelper {
     }
 
     public double calculate(){
-        return 0;
+        double pensionFactor = calculatePension();
+        double creditPointsFactor = calculateCreditPoints();
+        double tax = calculateTax();
+        double oneMonthSalary = baseSalary / 12;
+        return oneMonthSalary - (pensionFactor + creditPointsFactor + tax);
+    }
+
+    private double calculateTax() {
+        double tax = 0;
+        switch(taxLevel()){
+            case 1:
+                tax = 0.1;
+                break;
+            case 2:
+                tax = 0.14;
+                break;
+            case 3:
+                tax = 0.2;
+                break;
+            case 4:
+                tax = 0.31;
+                break;
+            case 5:
+                tax = 0.35;
+                break;
+            case 6:
+                tax = 0.47;
+                break;
+            case 7:
+                tax = 0.5;
+                break;
+        }
+        return (baseSalary * tax) / 12;
+    }
+
+    private int taxLevel() {
+        int firstLevel = 81_480;
+        int secondLevel = 116_760;
+        int thirdLevel = 187_440;
+        int fourthLevel = 260_520;
+        int fifthLevel = 542_160;
+        int sixthLevel = 698_280;
+        if (baseSalary <= firstLevel)
+            return 1;
+        if (baseSalary > firstLevel && baseSalary <= secondLevel)
+            return 2;
+        if (baseSalary > secondLevel && baseSalary <= thirdLevel)
+            return 3;
+        if (baseSalary > thirdLevel && baseSalary <= fourthLevel)
+            return 4;
+        if (baseSalary > fourthLevel && baseSalary <= fifthLevel)
+            return 5;
+        if (baseSalary > fifthLevel && baseSalary <= sixthLevel)
+            return 6;
+        if (baseSalary > sixthLevel)
+            return 7;
+        return 1;
+    }
+
+    private double calculateCreditPoints() {
+        return ((creditPoints / 100) * baseSalary) / 12;
+    }
+
+    private double calculatePension() {
+        return ((pension / 100) * baseSalary) / 12;
     }
 }
+
